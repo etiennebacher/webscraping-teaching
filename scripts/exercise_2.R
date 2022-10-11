c8c0052e4e00630886848a072b0b2d76f8b2cca6
etiennebacher <etienne.bacher@protonmail.com>
2022-10-11 14:05:03 +0200

    minor

--- scripts/exercise_2.R
+++ scripts/exercise_2.R
@@ -5,8 +5,6 @@ library(RSelenium)
 library(logger)
 
 
-# system("taskkill /im java.exe /f", intern=FALSE, ignore.stdout=FALSE)
-
 link <- "http://www.inci.org.br/acervodigital/livros.php"
 
 # Automatically go the website
@@ -27,10 +25,7 @@ button_element$clickElement()
 
 
 # Wait 5 sec for the page to load and then click on the button to open the modal
-# Sys.sleep(5)
-
-# find all modal buttons and highlight them one by one
-
+Sys.sleep(5)
 
 log_appender(appender_file("data/modals/00_logfile"))
 log_messages()
@@ -92,6 +87,8 @@ for (page_index in 1:2348) {
   }
 
   message(paste("Finished scraping of page", page_index))
+
+  Sys.sleep(2)
 
 }
 
